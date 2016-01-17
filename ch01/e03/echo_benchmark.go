77ae3a59dc2847d963b59c0889fefbb13d4ec3b1
Alberto Cortés <alberto@sourced.tech>
2016-01-17 19:58:56 +0100

    [c01-e03] Adds report of the confidence intervals of the mean duration

--- ch01/e03/echo_benchmark.go
+++ ch01/e03/echo_benchmark.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
+
+	"github.com/alcortesm/sample"
 )
 
 const (
 	inputSize       = 1000
-	runs            = 100000
+	runs            = 1000
 	confidenceLevel = 0.95
 )
 
@@ -17,31 +20,55 @@ func main() {
 		input[i] = fmt.Sprint(i)
 	}
 
-	fmt.Printf("mean concat execution time (%d elements, %0.2f%% confidence level)\n", inputSize, confidenceLevel)
-	fmt.Printf("v1\t%s\n", benchmark(v1, input))
+	durationConfidenceIntervals, err := benchmark(v1, input)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("mean concat execution time (%d elements, %d runs, %0.2f%% confidence level)\n",
+		inputSize, runs, confidenceLevel)
+	fmt.Printf("v1\t[ %s - %s ]\n", durationConfidenceIntervals[0], durationConfidenceIntervals[1])
+	os.Exit(0)
 }
 
-// TODO: make this return means according to confidence level
-func benchmark(f concat, input []string) time.Duration {
-	var total time.Duration
-	var start time.Time
-	var elapsed time.Duration
+// Runs `f` function over `input` a number of times (`runs`) and
+// calculates the confidence intervals of the mean duration of each run
+// for a `confidence` confidence level.
+func benchmark(f concat, input []string) ([2]time.Duration, error) {
+	var execTimes = make([]float64, runs)
 	for i := 0; i < runs; i++ {
-		start = time.Now()
+		start := time.Now()
 		f(input)
-		elapsed = time.Since(start)
-		total += elapsed
+		elapsed := time.Since(start)
+		execTimes[i] = float64(elapsed)
 	}
-	return time.Duration(int64(total) / int64(runs))
+
+	durations, err := sample.New(execTimes)
+	if err != nil {
+		return [2]time.Duration{}, err
+	}
+
+	meanConfidenceIntervals, err := durations.MeanConfidenceIntervals(confidenceLevel)
+	if err != nil {
+		return [2]time.Duration{}, err
+	}
+
+	asDuration := [2]time.Duration{
+		time.Duration(meanConfidenceIntervals[0]),
+		time.Duration(meanConfidenceIntervals[1]),
+	}
+
+	return asDuration, nil
 }
 
 type concat func([]string) string
 
-func v1(ss []string) (s string) {
+func v1(input []string) (output string) {
 	var sep string
-	for i := 0; i < len(ss); i++ {
-		s += sep + ss[i]
+	for i := 0; i < len(input); i++ {
+		output += sep + input[i]
 		sep = " "
 	}
-	return s
+	return output
 }
