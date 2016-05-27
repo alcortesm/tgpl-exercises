304aedcaa8e545fc17a40030da6bf3a15aa7a8cb
Alberto Cortés <alberto@sourced.tech>
2016-05-27 17:10:38 +0200

    ch01/e06 chose color based on the radius of the point

---
+++ ch01/e06/lissajous.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var palette = []color.Color{
+	color.RGBA{0x00, 0x00, 0x00, 0xFF},
+	color.RGBA{0x00, 0x00, 0xFF, 0xFF},
+	color.RGBA{0x00, 0xFF, 0x00, 0xFF},
+	color.RGBA{0x00, 0xFF, 0xFF, 0xFF},
+	color.RGBA{0xFF, 0x00, 0x00, 0xFF},
+	color.RGBA{0xFF, 0x00, 0xFF, 0xFF},
+	color.RGBA{0xFF, 0xFF, 0x00, 0xFF},
+	color.RGBA{0xFF, 0xFF, 0xFF, 0xFF},
+}
+
+const backgroundIndex = 0
+
+const (
+	output  = "/tmp/output.gif"
+	cycles  = 4     // number of complete x oscillator revolutions
+	res     = 0.001 // angular resolution
+	side    = 400   // image canvas side in pixels [0..side]
+	nframes = 64    // number of animation frames
+	delay   = 8     // delay between frames in 10ms units
+)
+
+func main() {
+	file, err := os.Create(output)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}()
+
+	randomSeedUsingTime()
+
+	if err := lissajous(file); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("output is at", output)
+}
+
+func randomSeedUsingTime() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func lissajous(out io.Writer) error {
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	phase := 0.0                 // phase difference
+	anim := gif.GIF{LoopCount: nframes}
+
+	for i := 0; i < nframes; i++ {
+		frame, delay := createFrame(anim, freq, phase)
+		anim.Image = append(anim.Image, frame)
+		anim.Delay = append(anim.Delay, delay)
+		phase += 1
+	}
+
+	return gif.EncodeAll(out, &anim)
+}
+
+func createFrame(anim gif.GIF, freq, phase float64) (*image.Paletted, int) {
+	rect := image.Rect(0, 0, side, side)
+	img := image.NewPaletted(rect, palette)
+
+	for t := 0.0; t < cycles*2*math.Pi; t += res {
+		x := math.Sin(t)
+		y := math.Sin(t*freq + phase)
+		px, py := cartesianToImage(x, y)
+		colorIndex := colorIndexFromRadius(px, py)
+		img.SetColorIndex(px, py, colorIndex)
+	}
+
+	return img, delay
+}
+
+func colorIndexFromRadius(x, y int) uint8 {
+	radius := distanceFromOrigin(x, y)
+	normalizedRadius := radius / (side * math.Sqrt(2.0))
+	index := normalizedRadius * float64(len(palette))
+
+	return uint8(math.Floor(index))
+}
+
+func distanceFromOrigin(x, y int) float64 {
+	return math.Sqrt(float64(x*x + y*y))
+}
+
+func cartesianToImage(x, y float64) (int, int) {
+	cX := (x + 1.0) * side / 2
+	cY := (-y + 1.0) * side / 2
+
+	return int(cX), int(cY)
+}
