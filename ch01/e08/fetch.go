b41838e035c04f5329a4f16d22c6aea990f691ae
Alberto Cortés <alberto@sourced.tech>
2016-05-27 17:43:02 +0200

    ch01/e08 ensure HTTP scheme

---
+++ ch01/e08/fetch.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		url = ensureHttpScheme(url)
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: copying %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
+
+const httpScheme = "http://"
+
+func ensureHttpScheme(url string) string {
+	if !strings.HasPrefix(url, httpScheme) {
+		url = httpScheme + url
+	}
+
+	return url
+}
